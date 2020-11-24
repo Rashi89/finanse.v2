b0fee3dbd91bbe87acc454f8da1d72e9d171cfe7
Rashi <agata.rachanska.programista@gmail.com>
2020-11-24 15:36:35 +0100

    add option remove expense category

--- App/Controllers/Expenses.php
+++ App/Controllers/Expenses.php
@@ -7,6 +7,7 @@ use App\Models\RememberedLogin;
 use \App\Models\Expense;
 use \Core\View;
 use \App\Auth;
+use \App\Flash;
 
 class Expenses extends Authenticated
 {
@@ -35,8 +36,8 @@ class Expenses extends Authenticated
 	
 	public function createAction()
 	{
-		$income= new Expense($_POST);
-		$income->saveExpense();
+		$expense= new Expense($_POST);
+		$expense->saveExpense();
 		$category =User::getAllExpenses($_SESSION['user_id']);
 		$payMethod=User::getAllPayMethod($_SESSION['user_id']);
 		View::renderTemplate('Expenses/add.html',[
@@ -44,5 +45,27 @@ class Expenses extends Authenticated
 			'category' => $category,
 			'payMethod'=>$payMethod
 		]);	
+	}
+	
+	public function showAction()
+	{
+		if(isset($_SESSION['user_id']))
+		{
+				$category =User::getAllExpenses($_SESSION['user_id']);
+				View::renderTemplate('Options/removeexpense.html',[
+				'user' => $this->user,
+				'category' => $category
+			
+					]);	
+		}
+
+	}
+	
+	public function removeAction()
+	{
+		$expense= new Expense($_POST);
+		$expense->removeExpense();
+		Flash::addMessage('Wybrana kategoria została usunięta!');
+		$this->redirect('/expenses/show');
 	}
 }
--- App/Controllers/Options.php
+++ App/Controllers/Options.php
@@ -132,4 +132,5 @@ class Options extends Authenticated
 		}
 	}
 	
+	
 }
